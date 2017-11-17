9b3fe430435ef0dbfdeea0f0a60a517b6c56ab59
nick spragg <nick.spragg@bbc.co.uk>
2017-11-17 10:55:35 +0000

    Task: Add error handling

--- __tests__/mapLimit.js
+++ __tests__/mapLimit.js
@@ -2,6 +2,7 @@
 
 const asyncify = require('../lib/asyncify');
 const double = require('./utils/functions');
+const ExecutorError = require('./../lib/ExecutorError');
 
 describe('.mapLimit', () => {
   it('map each element', async () => {
@@ -25,23 +26,18 @@ describe('.mapLimit', () => {
     expect(results).toEqual([]);
   });
 
-  it('limit in progress', async () => {
+  it('limits the numbers of invokes at a given time', async () => {
     const limit = 2;
     const running = [];
 
     const results = await asyncify.mapLimit(
       [10, 20, 30, 40, 50, 60],
-      async number => {
+      async (number) => {
         running.push(number);
-
-        const active = running.length;
-        if (active > 1) {
-          expect(active).toBeLessThanOrEqual(limit);
-        }
-        return double(number).then(doubled => {
-          running.pop();
-          return doubled;
-        });
+        expect(running.length).toBeLessThanOrEqual(limit);
+        const doubled = await double(number);
+        running.pop();
+        return doubled;
       },
       limit
     );
@@ -49,9 +45,46 @@ describe('.mapLimit', () => {
     expect(results).toEqual([20, 40, 60, 80, 100, 120]);
   });
 
-  it('limit <');
-  it('limit >');
-  it('error');
+  it('does not make more invocations that arr length', async () => {
+    const input = [10, 20, 30, 40, 50, 60];
+    const limit = input.length + 10;
+    const running = [];
+
+    const results = await asyncify.mapLimit(
+      input,
+      async (number) => {
+        running.push(number);
+        expect(running.length).toBeLessThanOrEqual(limit);
+        const doubled = await double(number);
+        running.pop();
+        return doubled;
+      },
+      limit
+    );
+
+    expect(results).toEqual([20, 40, 60, 80, 100, 120]);
+  });
+
+  it('has a default concurrency level of 1');
+
+  it('returns an error if at least one invocation fails', async () => {
+    let calls = 0;
+    try {
+      await asyncify.mapLimit(
+        [10, 0, 30],
+        (n) => {
+          calls++;
+          if (n === 0) throw new Error('div by zero');
+          return 10 / n;
+        },
+        1
+      );
+    } catch (err) {
+      expect(err).toBeInstanceOf(ExecutorError);
+      expect(calls).toBe(2);
+    }
+  });
+
   it('map');
   it('set');
 });
